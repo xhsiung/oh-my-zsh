6eb4d1df4a70b4b267d5b741e342b2a4558b327d
xhsiung <xhsiung@gmail.com>
2020-11-28 10:54:08 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+sudo apt-get install fonts-powerline
+fc-cache -f -v
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k

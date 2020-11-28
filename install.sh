d47a8f58e3b7b8068383c8daa33794774aa88487
xhsiung <xhsiung@gmail.com>
2020-11-28 10:51:53 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
